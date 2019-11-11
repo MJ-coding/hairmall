801a48985076472fb8b04e76f84eb27380bf6c2e
MJ-coding <zmffheldk@gmail.com>
2019-11-11 18:47:28 +0900

    initial commit

---
+++ HairMall1/hairmall2/src/main/java/com/example/hairmall2/ui/act11_stylesearchingm.java
@@ -0,0 +1,16 @@
+package com.example.hairmall2.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hairmall2.R;
+
+public class act11_stylesearchingm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act11_stylesearchingm);
+    }
+}
---
+++ HairMall1/hairmall2/src/main/java/com/example/hairmall2/ui/home/act1_mainpage.java
@@ -0,0 +1,35 @@
+package com.example.hairmall2.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.hairmall2.R;
+
+public class act1_mainpage extends Fragment {
+
+    private act6_mainpageviewmodel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(act6_mainpageviewmodel.class);
+        View root = inflater.inflate(R.layout.act1_mainpage, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
