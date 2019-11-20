e69c62b01d71a2adc3517c0759d816489d2822b7
rifkehtm6 <ejengkdy@gamil.com>
2019-11-21 00:29:09 +0900

    m

---
+++ HairMall1/hairmall2/src/main/java/com/example/hairmall2/ui/act3_login.java
@@ -0,0 +1,16 @@
+package com.example.hairmall2.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hairmall2.R;
+
+public class act3_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act3_login);
+    }
+}
---
+++ HairMall1/hairmall2/src/main/java/com/example/hairmall2/ui/act5_mypage.java
@@ -0,0 +1,16 @@
+package com.example.hairmall2.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hairmall2.R;
+
+public class act5_mypage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act5_mypage);
+    }
+}
